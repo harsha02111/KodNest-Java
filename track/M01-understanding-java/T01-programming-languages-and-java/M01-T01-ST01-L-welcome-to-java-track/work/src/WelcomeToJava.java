63873a626419274fcab86f39d63d5d34fa85172a
Harshavardhan <kalikapuramharshavardhan@gmail.com>
2026-07-30 17:30:09 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,6 @@ public class WelcomeToJava {
        System.out.println("Welcome to the Java Track");
        System.out.println("I know already how to build programming logic");
        System.out.println("Now I am learning how to express that logic using Java.");
+       System.out.println("Object oriented Programming");
     }
 }
