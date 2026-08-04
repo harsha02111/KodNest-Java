521cf6b5f5c1044a8efe25eed19484f35a64410a
Harshavardhan <kalikapuramharshavardhan@gmail.com>
2026-08-04 11:32:53 +0530

    Day3

---
+++ track/M02/T01/ST01/P01/Demo11.java
@@ -0,0 +1,11 @@
+public class Demo11 {
+	public static void main(String[] args) {
+		System.out.println("Monday");
+		System.out.print("Tuesday");
+		System.out.println("wednesday");
+		System.out.println("Thursday");
+		System.out.print("Friday");
+		System.out.println("Saturday");
+		System.out.println("Sunday");
+	}
+}
