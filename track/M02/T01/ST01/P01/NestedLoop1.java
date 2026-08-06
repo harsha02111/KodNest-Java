0fb0eddc4eea763a14d9c3b7a360a021df16a4f6
Harshavardhan <kalikapuramharshavardhan@gmail.com>
2026-08-06 11:45:28 +0530

    Nested Loops added

---
+++ track/M02/T01/ST01/P01/NestedLoop1.java
@@ -0,0 +1,10 @@
+public class NestedLoop1 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 6; i++) {
+			for (int j = 1; j < 6; j++) {
+				System.out.println(j);
+			}
+			System.out.println();
+		}
+	}
+}
