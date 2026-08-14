b536daeacd5e59325b1cf765c1ff4efc244d029e
Harshavardhan <kalikapuramharshavardhan@gmail.com>
2026-08-14 11:57:08 +0530

    Constructor Overloading

---
+++ track/M02/T01/ST01/P01/Robot1.java
@@ -0,0 +1,13 @@
+class Robot {
+    Robot() {
+        // Print the message
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class Robot1 {
+    public static void main(String[] args) {
+        // Create one Robot object
+        Robot r=new Robot();
+    }
+}
